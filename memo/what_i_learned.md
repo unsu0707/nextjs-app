6d155e9ac5ca76a72777e30d828ab279ff3b5c0b
unsu0707 <unsu0707@gmail.com>
2022-01-07 16:08:38 +0900

    Add counter button to index.js for checking pre-rendering

--- memo/what_i_learned.md
+++ memo/what_i_learned.md
@@ -1,3 +1,7 @@
 ### 1. pages
 
 - HTML に Render するためには export default function からユーザーに見せたいものを Return する
+
+### 2. Static Pre-Rendering
+
+- Next.js は Pre-rendering を対応する。Client-Side で Rendering しなくても、初期状態のページが Render される。
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { useState } from "react";
+
 export default function Home() {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <h1>Hello, World-{counter}!</h1>
+      <button onClick={() => setCounter((prev) => prev + 1)}></button>
     </div>
   );
 }
