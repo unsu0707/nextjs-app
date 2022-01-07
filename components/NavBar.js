5fd6cacc56e38855796b6a15c9e956a5b1c1ca14
unsu0707 <unsu0707@gmail.com>
2022-01-07 16:27:16 +0900

    use next/router for use router infoo

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,13 +1,17 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function NavBar() {
+  const router = useRouter();
   return (
     <nav>
       <Link href="/">
-        <a>Home</a>
+        <a style={{ color: router.pathname === "/" ? "red" : "blue" }}>Home</a>
       </Link>
       <Link href="/about">
-        <a>About</a>
+        <a style={{ color: router.pathname === "/about" ? "red" : "blue" }}>
+          About
+        </a>
       </Link>
     </nav>
   );
