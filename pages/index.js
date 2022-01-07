aaaaeb4adee4a458c786d02d9d53289e78a54883
unsu0707 <unsu0707@gmail.com>
2022-01-07 16:21:25 +0900

    Add NavBar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import NavBar from "../components/NavBar";
 
 export default function Home() {
   const [counter, setCounter] = useState(0);
 
   return (
     <div>
+      <NavBar />
       <h1>Hello, World-{counter}!</h1>
       <button onClick={() => setCounter((prev) => prev + 1)}></button>
     </div>
