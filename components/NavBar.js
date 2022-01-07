60bc0562bf4c3a29a8b02d6faa7b046a091c10b6
unsu0707 <unsu0707@gmail.com>
2022-01-07 16:22:47 +0900

    use next/link for linked anchor tags

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <nav>
-      <a href="/">Home</a>
-      <a href="/about">About</a>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
     </nav>
   );
 }
--- memo/what_i_learned.md
+++ memo/what_i_learned.md
@@ -5,3 +5,7 @@
 ### 2. Static Pre-Rendering
 
 - Next.js は Pre-rendering を対応する。Client-Side で Rendering しなくても、初期状態のページが Render される。
+
+### 3. Link
+
+- リンクを持つ anchor tag は next/link(Link)を使う
