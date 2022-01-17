05097dcf347a5e6ce8d4fb3e3d17168d911ba4eb
unsu0707 <unsu0707@gmail.com>
2022-01-17 15:27:58 +0900

    New css for NavBar

--- components/NavBar.js
+++ components/NavBar.js
@@ -13,13 +13,29 @@ export default function NavBar() {
       </Link>
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
