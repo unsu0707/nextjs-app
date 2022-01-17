0d7cc974029e4c95a8690d5eea2d0ffd3e555cd6
unsu0707 <unsu0707@gmail.com>
2022-01-17 15:17:32 +0900

    use Head

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import { useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
-
   return (
     <div>
-      <h1 className="active">Hello, World-{counter}!</h1>
-      <button onClick={() => setCounter((prev) => prev + 1)}></button>
+      <Head>
+        <title>Home | Next Movies</title>
+      </Head>
+      <h1 className="active">Hello</h1>
     </div>
   );
 }
